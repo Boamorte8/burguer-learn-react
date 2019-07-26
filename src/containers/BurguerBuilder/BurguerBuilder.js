6ef628cbed7a0160a1a0ba1fd630c2c4df085491
Esteban Salazar Zapata <esteban.salazar@Mac-mini-de-administrador.local>
2019-07-26 17:18:24 -0500

    Added price

--- src/containers/BurguerBuilder/BurguerBuilder.js
+++ src/containers/BurguerBuilder/BurguerBuilder.js
@@ -1,18 +1,17 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
-import Aux from '../../hoc/Aux';
-import Burger from '../../components/Burger/Burger';
-import BuildControls from '../../components/Burger/BuildControls/BuildControls';
+import Aux from "../../hoc/Aux";
+import Burger from "../../components/Burger/Burger";
+import BuildControls from "../../components/Burger/BuildControls/BuildControls";
 
 const INGREDIENT_PRICES = {
   salad: 0.5,
   cheese: 0.4,
   meat: 1.3,
   bacon: 0.7
-}
+};
 
 class BurguerBuilder extends Component {
-
   // constructor(props) {
   //   super(props);
   //   this.state = {}
@@ -26,11 +25,11 @@ class BurguerBuilder extends Component {
       meat: 0
     },
     totalPrice: 4
-  }
+  };
 
-  addIngredientHandler = (type) => {
+  addIngredientHandler = type => {
     const oldCount = this.state.ingredients[type];
-    const updatedCount  = oldCount+1;
+    const updatedCount = oldCount + 1;
     const updatedIngredients = {
       ...this.state.ingredients
     };
@@ -38,13 +37,13 @@ class BurguerBuilder extends Component {
     const priceAddittion = INGREDIENT_PRICES[type];
     const oldPrice = this.state.totalPrice;
     const newPrice = oldPrice + priceAddittion;
-    this.setState({totalPrice: newPrice, ingredients: updatedIngredients});
-  }
+    this.setState({ totalPrice: newPrice, ingredients: updatedIngredients });
+  };
 
-  removeIngredientHandler = (type) => {
+  removeIngredientHandler = type => {
     const oldCount = this.state.ingredients[type];
     if (oldCount > 0) {
-      const updatedCount  = oldCount-1;
+      const updatedCount = oldCount - 1;
       const updatedIngredients = {
         ...this.state.ingredients
       };
@@ -52,15 +51,27 @@ class BurguerBuilder extends Component {
       const priceDeduction = INGREDIENT_PRICES[type];
       const oldPrice = this.state.totalPrice;
       const newPrice = oldPrice - priceDeduction;
-      this.setState({totalPrice: newPrice, ingredients: updatedIngredients});
+      this.setState({ totalPrice: newPrice, ingredients: updatedIngredients });
     }
-  }
+    return;
+  };
 
   render() {
+    const disabledInfo = {
+      ...this.state.ingredients
+    };
+    for (let key in disabledInfo) {
+      disabledInfo[key] = disabledInfo[key] <= 0;
+    }
     return (
       <Aux>
         <Burger ingredients={this.state.ingredients} />
-        <BuildControls ingredientAdded={this.addIngredientHandler} ingredientRemoved={this.removeIngredientHandler} />
+        <BuildControls
+          ingredientAdded={this.addIngredientHandler}
+          ingredientRemoved={this.removeIngredientHandler}
+          disabled={disabledInfo}
+          price={this.state.totalPrice}
+        />
       </Aux>
     );
   }
